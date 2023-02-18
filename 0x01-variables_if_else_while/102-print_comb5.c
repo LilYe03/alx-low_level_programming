db5d656b9ecc19e0da35520f9b8813b4c48574c2
Emmanuel Johnson <emmajohnsonye@icloud.com>
2023-02-17 17:02:26 -0800

    jk

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -9,31 +8,26 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
-
+	for (i = 0; i < 100; i++)
 	{
-
-		for (j = i; j <= 99; j++)
+		for (j = i + 1; j < 100; j++)
 		{
-			int d1, d2, d3, d4;
-
-			d1 = i / 10;
-			d2 = i % 10;
-			d3 = j / 10;
-			d4 = j % 10;
-			putchar(d1 + '0');
-			putchar(d2 + '0');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-			putchar(d3 + '0');
-			putchar(d4 + '0');
-			if (i != 99 || j != 99)
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 98 && j == 99)
+			{
+				putchar('\n');
+			}
+			else
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-	putchar('\n');
-
 	return (0);
 }
