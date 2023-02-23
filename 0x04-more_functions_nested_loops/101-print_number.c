249ece7541bb879594a47a6ed85551f3ffa8edd8
Emmanuel Johnson <emmajohnsonye@icloud.com>
2023-02-23 15:46:44 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,7 +5,7 @@
  * print_number - Prints an integer
  * @n: The integer to print
  *
- * Return: Nothing!
+ * Return: void
  */
 
 void print_number(int n)
@@ -15,12 +15,13 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		n *= -1;
-		k = n;
 		_putchar('_');
+		n = -n;
 	}
-	k /= 10;
-	if (k != 0)
-		print_number(k);
-	_putchar((unsigned int) n % 10 + '0');
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
 }
